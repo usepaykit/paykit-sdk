5ee65b8b446166ea1d037ad3bc0d4f551d1d1521
devodii <emmanuelodii80@gmail.com>
2025-07-05 22:42:46 +0100

    feat: added shadcn ui to react app

--- examples/react/src/main.jsx
+++ examples/react/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Homepage } from './pages/home';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Homepage />
   </StrictMode>,
-)
+);
---
+++ examples/react/src/pages/home.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const Homepage = () => {
+  return <div className="text-red-500">Homepage</div>;
+};

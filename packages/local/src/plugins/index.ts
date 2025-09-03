26ca0abc6c921d9d5e067af93e266d0f7f61b28a
devodii <emmanuelodii80@gmail.com>
2025-09-03 20:31:44 +0100

    delete: remove paykit-summary.txt file containing payment system overview and rationale

--- packages/local/src/plugins/index.ts
+++ packages/local/src/plugins/index.ts
@@ -2,8 +2,8 @@ import { Webhook } from '@paykit-sdk/core';
 import { server$HandleWebhook } from '../server';
 
 /**
- * Generic webhook handler for the local provider
- * Works with any framework that can pass a URL and webhook instance
+ * @description Handles webhooks for the local provider by processing the URL and webhook instance.
+ * Can be used with any framework that provides these parameters.
  */
 export const withLocalWebhook = async (url: string, webhook: Webhook) => {
   return await server$HandleWebhook(url, webhook);

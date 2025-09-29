3a2cf33c698480e38064dacd502d63ae4166824d
devodii <emmanuelodii80@gmail.com>
2025-09-29 12:30:01 +0100

    fix: env variable template

--- packages/comgate/src/index.ts
+++ packages/comgate/src/index.ts
@@ -6,9 +6,12 @@ export const createComgate = (config: ComgateConfig) => {
 };
 
 export const comgate = () => {
-  const envVars = validateEnvVars(['COMGATE_API_KEY'], process.env);
+  const envVars = validateEnvVars(['COMGATE_MERCHANT', 'COMGATE_SECRET', 'COMGATE_BASE_URL', 'COMGATE_SANDBOX'], process.env);
 
-  const apiKey = envVars.COMGATE_API_KEY;
+  const merchant = envVars.COMGATE_MERCHANT;
+  const secret = envVars.COMGATE_SECRET;
+  const baseUrl = envVars.COMGATE_BASE_URL;
+  const sandbox = envVars.COMGATE_SANDBOX === 'true';
 
-  return createComgate({ apiKey });
+  return createComgate({ merchant, secret, baseUrl, sandbox });
 };

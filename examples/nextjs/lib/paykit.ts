f8c11df3e6a75b1443a2ef2ce4900e1ec7b84c7d
devodii <emmanuelodii80@gmail.com>
2025-10-12 13:01:18 +0100

    chore: removed example nextjs app

--- examples/nextjs/lib/paykit.ts
+++
@@ -1,9 +0,0 @@
-import { PayKit } from '@paykit-sdk/core';
-import { createLocal } from '@paykit-sdk/local';
-
-export const provider = createLocal({
-  webhookUrl: 'http://localhost:3000/api/paykit',
-  paymentUrl: 'http://localhost:3001',
-});
-
-export const paykit = new PayKit(provider);
